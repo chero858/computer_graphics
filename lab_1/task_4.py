bbdabd3ce8c8f4ddaa31b78110c6b03d7963035f
Roman Cherkasov <chero858@gmail.com>
2020-02-15 18:51:36 +0300

    edit task_4 in lab_1

--- lab_1/task_4.py
+++ lab_1/task_4.py
@@ -1,9 +1,12 @@
 from task_1 import determine_the_location_of_the_point
-from task_3 import draw_polygon
+from task_3 import draw_polygon, is_polygon_simple
 
 
 def is_polygon_convex(*points):
-    points += (points[0], points[1])
+    points += (points[0],)
+    if not is_polygon_simple(points):
+        return False
+    points += (points[1],)
     location = determine_the_location_of_the_point(points[0], points[1], points[2])
     for i, point in enumerate(points[:len(points) - 2]):
         if location != determine_the_location_of_the_point(points[i], points[i + 1], points[i + 2]):
